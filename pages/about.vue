4a51b0bcb57043cd27e12d87ace2ad00052956a7
Peggy535 <peggy161152@gmail.com>
2022-09-18 12:07:52 +0100

    Clodinary plugin added

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,60 @@
 <template>
-  <h1 class="text-black">About JH</h1>
+  <main>
+    <section class="flex flex-col h-screen w-screen justify-end">
+      <div class="flex h-auto my-auto text-left">
+        <h1
+          class="flex text-6xl m-2 p-2 text-brand-rose-gold font-Domainemedium"
+        >
+          About JH
+        </h1>
+      </div>
+      <blockquote
+        class="h-auto col-span-1 row-span-1 rounded-3xl m-1 p-2 bg-brand-rose-gold text-neutral-50 border-4 border-neutral-50 shadow-lg shadow-neutral-400"
+      >
+        <p
+          class="text-2xl leading-relaxed m-1 p-1 my-14 font-Domainelight border-l-4 border-l-neutral-50 border-spacing-10"
+        >
+          <span
+            class="text-7xl sm:text-13xl md:text-15xl lg:text-18xl font-Domainemedium"
+            >Having</span
+          >
+          personally experienced such things as anxiety, trauma and chronic
+          fatigue syndrome, and having recovered through the same therapies I
+          offer today, allows me to truly understand other's issue's, and how
+          best to treat them."
+        </p>
+        <cite class="m-1 p-1">- Justine Hodgson</cite>
+      </blockquote>
+    </section>
+    <section class="h-screen w-screen flex flex-col justify-evenly">
+      <p class="m-2 p-2 text-xl leading-normal">
+        By now hopefully, you’ve pressed the play button on both my welcome and
+        about me recordings and I’ve saved you valuable time on reading a page
+        full of words without much connection. As a hypnotherapist, it’s
+        important you get a feel for my voice and personality if we are going to
+        do some work together. I’m also a strong believer in being as real on
+        paper as I am in person, hopefully this all resonates and feels
+        authentic for every visitor to my site.
+      </p>
+      <img
+        class="object-contain object-center"
+        src="images/journey_path.jpg"
+        alt=""
+      />
+    </section>
+    <section class="h-screen w-screen flex flex-col justify-evenly">
+      <p class="m-2 p-2 text-xl leading-normal">
+        At this stage you would have heard my story and know, just as so many
+        before me and many to follow, I’ve had my own path to travel and it’s
+        not always been straight forward but it has led me to where I am today
+        and it continues to get better and convince me that, miracles actually
+        do happen, every single day.
+      </p>
+      <img
+        class="object-contain object-center"
+        src="images/lady_field_sun_background.jpg"
+        alt=""
+      />
+    </section>
+  </main>
 </template>
