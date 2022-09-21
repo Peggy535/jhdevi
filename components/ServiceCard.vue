6097386aebfa885766e3b2b1dc68db6f09bc79d7
Peggy535 <peggy161152@gmail.com>
2022-09-21 18:02:51 +0100

    Service cards added for mobile only so far

---
+++ components/ServiceCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <NuxtLink to="/" class="group">
+    <article
+      class="border-2 border-neutral-200 rounded-2xl shadow-2xl shadow-neutral-400"
+    >
+      <img
+        :src="artwork.displayImageUrl"
+        class="rounded-t-xl shadow-2xl shadow-neutral-300 object-contain object-center"
+        loading="lazy"
+      />
+
+      <div class="px-3 py-2">
+        <h1 class="text-lg p-1">
+          {{ artwork.title }}
+        </h1>
+        <h2 class="font-MontrealLight text-md p-1">
+          Pattern No. {{ artwork.patternNumber }}
+        </h2>
+        <h2 class="font-MontrealLight text-md p-1">
+          {{ artwork.sizeInches }} |
+          {{ artwork.sizeCm }}
+        </h2>
+        <h2 class="font-MontrealLight text-md p-1">
+          {{ artwork.subTitle }}
+        </h2>
+        <h3 class="font-MontrealLight text-md p-1">
+          Artwork Edition No: {{ artwork.sku }}
+        </h3>
+        <p class="p-1 text-3xl">£{{ artwork.price }}</p>
+      </div>
+    </article>
+  </NuxtLink>
+</template>
+
+<script setup>
+const route = useRoute();
+const props = defineProps({
+  artwork: Object,
+});
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -93,5 +93,97 @@
       wide, as far as the imagination can take you!
     </p>
   </section>
-  <section></section>
+  <section class="h-auto w-screen">
+    <div class="flex">
+      <h1 class="font-Domainemedium text-6xl m-1 p-1">JH Services</h1>
+    </div>
+    <div class="grid grid-cols-1">
+      <article
+        class="grid col-span-1 min-h-min m-2 p-2 border-2 border-neutral-200 rounded-2xl shadow-2xl shadow-neutral-600"
+      >
+        <span class="h-80 bg-brand-orange rounded-2xl">
+          <h1 class="font-Domainemedium text-8xl text-neutral-600 m-2 p-2">
+            CBT
+          </h1>
+        </span>
+        <span>
+          <p class="font-ObjectSans my-20 text-xl">
+            Cognitive behavioural therapy (CBT) is a talking therapy that can
+            help you manage your problems by changing the way you think and
+            behave.
+          </p>
+          <button
+            class="border-2 border-neutral-800 m-2 p-2 rounded-2xl font-ObjectsSans text-2xl"
+          >
+            Learn more
+          </button>
+        </span>
+      </article>
+      <article
+        class="grid col-span-1 min-h-min m-2 p-2 border-2 border-neutral-200 rounded-2xl shadow-2xl shadow-neutral-600"
+      >
+        <span class="h-80 bg-brand-yellow rounded-2xl">
+          <h1 class="font-Domainemedium text-5xl text-neutral-600 m-2 p-2">
+            Hypnotherapy
+          </h1>
+        </span>
+        <span>
+          <p class="font-ObjectSans my-20 text-xl">
+            Curative hypnotherapy is a method of working purely with the
+            subconscious mind to understand and correct specific details which
+            have played a part in the initial creation of a symptom.
+          </p>
+          <button
+            class="border-2 border-neutral-800 m-2 p-2 rounded-2xl font-ObjectsSans text-2xl"
+          >
+            Learn more
+          </button>
+        </span>
+      </article>
+      <article
+        class="grid col-span-1 min-h-min m-2 p-2 border-2 border-neutral-200 rounded-2xl shadow-2xl shadow-neutral-900"
+      >
+        <span class="h-80 bg-brand-green rounded-2xl">
+          <h1 class="font-Domainemedium text-8xl text-neutral-600 m-2 p-2">
+            EFT
+          </h1>
+          <p class="font-Domainelight text-lg m-2 p-2 text-neutral-600">
+            Emotional Freedom Technique
+          </p>
+        </span>
+        <span>
+          <p class="font-ObjectSans my-20 text-xl">
+            Cognitive behavioural therapy (CBT) is a talking therapy that can
+            help you manage your problems by changing the way you think and
+            behave.
+          </p>
+          <button
+            class="border-2 border-neutral-800 m-2 p-2 rounded-2xl font-ObjectsSans text-2xl"
+          >
+            Learn more
+          </button>
+        </span>
+      </article>
+      <article
+        class="grid col-span-1 min-h-min m-2 p-2 border-2 border-neutral-200 rounded-2xl shadow-2xl shadow-neutral-900"
+      >
+        <span class="h-80 bg-brand-blue rounded-2xl">
+          <h1 class="font-Domainemedium text-8xl text-neutral-600 m-2 p-2">
+            Yin Yoga
+          </h1>
+        </span>
+        <span>
+          <p class="font-ObjectSans my-20 text-xl">
+            An instant stress relief button! Yin is the most accessible of the
+            physical yoga systems and can be practised by anyone.
+          </p>
+          <button
+            class="border-2 border-neutral-800 m-2 p-2 rounded-2xl font-ObjectsSans text-2xl"
+          >
+            Learn more
+          </button>
+        </span>
+      </article>
+    </div>
+  </section>
 </template>
