0e59feeb1a2d5273bb2cb034eebae662501e0065
Peggy535 <peggy161152@gmail.com>
2022-09-21 17:04:57 +0100

    Changes to index and about pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,87 +1,97 @@
 <template>
-  <main>
-    <section class="flex flex-col h-screen w-screen justify-end">
-      <div class="flex h-auto my-auto text-left">
-        <h1
-          class="flex text-7xl sm:text-8xl md:text-9xl m-1 p-1 text-brand-rose-gold font-Domainemedium"
-        >
-          About JH
-        </h1>
-      </div>
-      <blockquote
-        class="h-auto w-full lg:w-3/4 lg:self-end rounded-3xl m-1 p-1 sm:m-3 sm:p-3 bg-brand-rose-gold text-neutral-50 border-2 border-neutral-200 shadow-lg shadow-neutral-400"
+  <section class="flex flex-col h-screen w-screen justify-end">
+    <div class="flex h-auto my-auto text-left">
+      <h1
+        class="flex text-7xl sm:text-8xl md:text-9xl m-1 p-1 text-brand-rose-gold font-Domainemedium"
       >
-        <p
-          class="text-2xl leading-relaxed m-1 p-1 my-20  border-l-4 border-l-neutral-50 border-spacing-10"
-        >
-          <span
-            class="m-1 p-1 text-8xl sm:text-10xl md:text-12xl lg:text-12xl font-Domainemedium"
-            >Having</span
-          >
-          personally experienced such things as anxiety, trauma and chronic
-          fatigue syndrome, and having recovered through the same therapies I
-          offer today, allows me to truly understand other's issue's, and how
-          best to treat them."
-        </p>
-        <cite class="m-1 p-1 font-Domainelight">- Justine Hodgson</cite>
-      </blockquote>
-    </section>
-    <section class="h-auto w-screen flex flex-col justify-evenly lg:grid lg:grid-cols-4">
-      <section
-        class="m-1 p-1 my-10 mx-auto overflow-hidden border-8 border-neutral-100 shadow-2xl shadow-neutral-300 rounded-2xl z-10 lg:grid lg:col-span-1"
+        About JH
+      </h1>
+    </div>
+    <blockquote
+      class="h-auto lg:w-3/4 lg:self-end rounded-3xl m-1 p-1 sm:m-3 sm:p-3 bg-brand-rose-gold text-neutral-50 border-2 border-neutral-200 shadow-lg shadow-neutral-400"
+    >
+      <p
+        class="text-xl leading-relaxed md:leading-loose m-1 p-1 my-10 md:my-16 border-l-4 border-l-neutral-50 border-spacing-10"
       >
-        <video controls class="z-5">
-          <source
-            src="https://res.cloudinary.com/peggy-co/video/upload/c_scale,w_300/v1593601416/12%20Steps%20The%20Curative%20Hypno/welcome_mess_a02ez5.mp4"
-            type="video/mp4"
-          />
-          <source
-            src="https://res.cloudinary.com/peggy-co/video/upload/c_scale,w_300/v1593601416/12%20Steps%20The%20Curative%20Hypno/welcome_mess_a02ez5.webm"
-            type="video/webm"
-          />
-        </video>
-        <cite class="text-md lg:text-sm font-Domainelight m-1 p-1"
-          >A personal message from Justine</cite
+        <span
+          class="text-8xl sm:text-10xl md:text-11xl lg:text-12xl font-Domainemedium"
+          >Having</span
         >
-      </section>
-      <p class="m-2 p-2 sm:m-4 sm:p-4 text-2xl leading-relaxed lg:grid lg:col-span-3 items-center">
-        By now hopefully, you’ve pressed the play button on both my welcome and
-        about me recordings and I’ve saved you valuable time on reading a page
-        full of words without much connection. As a hypnotherapist, it’s
-        important you get a feel for my voice and personality if we are going to
-        do some work together. I’m also a strong believer in being as real on
-        paper as I am in person, hopefully this all resonates and feels
-        authentic for every visitor to my site.
+        personally experienced such things as anxiety, trauma and chronic
+        fatigue syndrome, and having recovered through the same therapies I
+        offer today, allows me to truly understand other's issue's, and how best
+        to treat them."
       </p>
+      <cite class="m-1 p-1 font-Domainelight">- Justine Hodgson</cite>
+    </blockquote>
+  </section>
+  <section
+    class="h-auto w-screen flex flex-col justify-evenly lg:grid lg:grid-cols-4"
+  >
+    <section
+      class="m-1 p-1 my-10 mx-auto overflow-hidden border-8 border-neutral-100 shadow-2xl shadow-neutral-300 rounded-2xl z-10 lg:grid lg:col-span-1"
+    >
+      <video controls class="z-5">
+        <source
+          src="https://res.cloudinary.com/peggy-co/video/upload/c_scale,w_300/v1593601416/12%20Steps%20The%20Curative%20Hypno/welcome_mess_a02ez5.mp4"
+          type="video/mp4"
+        />
+        <source
+          src="https://res.cloudinary.com/peggy-co/video/upload/c_scale,w_300/v1593601416/12%20Steps%20The%20Curative%20Hypno/welcome_mess_a02ez5.webm"
+          type="video/webm"
+        />
+      </video>
+      <cite class="text-md lg:text-sm font-Domainelight m-1 p-1"
+        >A personal message from Justine</cite
+      >
     </section>
-    <section class="h-screen w-screen flex flex-col justify-evenly lg:grid lg:grid-cols-4 lg:grid-rows-1 lg:h-auto lg:items-center">
-      <img
-        class="object-contain object-center lg:col-start-3 lg:col-end-5 lg:row-start-1 lg:row-end-2"
-        src="https://res.cloudinary.com/peggy-co/image/upload/f_auto,q_auto/v1596119487/Well%20Being/journey_eaxuxe.jpg"
-        alt=""
-      />
-      <p class="m-2 p-2 sm:m-4 sm:p-4 text-2xl leading-relaxed lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-2">
-        At this stage you would have heard my story and know, just as so many
-        before me and many to follow, I’ve had my own path to travel and it’s
-        not always been straight forward but it has led me to where I am today
-        and it continues to get better and convince me that, miracles actually
-        do happen, every single day.
-      </p>
-    </section>
-    <section class="h-screen w-screen flex flex-col justify-evenly lg:grid lg:grid-cols-4 lg:grid-rows-1 lg:h-auto lg:items-center">
-      <img
-        class="object-contain object-center lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-2"
-        src="https://res.cloudinary.com/peggy-co/image/upload/f_auto,q_auto/v1596125701/Well%20Being/lady-field.min_jidrzu.jpg"
-        alt=""
-      />
-      <p class="m-2 p-2 sm:m-4 sm:p-4 text-2xl leading-relaxed lg:col-start-3 lg:col-end-5 lg:row-start-1 lg:row-end-2">
-        I’m that guide. My desire today and every day hereon, is to make people
-        feel less broken, so they can experience more joy and happiness and less
-        stress in their lives. This in turn can only benefit those around them,
-        and there we have it, a beautiful ripple effect begins and reaches far
-        and wide, as far as the imagination can take you!
-      </p>
-    </section>
-
-  </main>
+    <p
+      class="m-2 p-2 sm:m-4 sm:p-4 text-2xl leading-relaxed lg:grid lg:col-span-3 items-center"
+    >
+      By now hopefully, you’ve pressed the play button on both my welcome and
+      about me recordings and I’ve saved you valuable time on reading a page
+      full of words without much connection. As a hypnotherapist, it’s important
+      you get a feel for my voice and personality if we are going to do some
+      work together. I’m also a strong believer in being as real on paper as I
+      am in person, hopefully this all resonates and feels authentic for every
+      visitor to my site.
+    </p>
+  </section>
+  <section
+    class="h-screen w-screen flex flex-col justify-evenly lg:grid lg:grid-cols-4 lg:grid-rows-1 lg:h-auto lg:items-center"
+  >
+    <img
+      class="object-contain object-center lg:col-start-3 lg:col-end-5 lg:row-start-1 lg:row-end-2"
+      src="https://res.cloudinary.com/peggy-co/image/upload/f_auto,q_auto/v1596119487/Well%20Being/journey_eaxuxe.jpg"
+      alt=""
+    />
+    <p
+      class="m-2 p-2 sm:m-4 sm:p-4 text-2xl leading-relaxed lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-2"
+    >
+      At this stage you would have heard my story and know, just as so many
+      before me and many to follow, I’ve had my own path to travel and it’s not
+      always been straight forward but it has led me to where I am today and it
+      continues to get better and convince me that, miracles actually do happen,
+      every single day.
+    </p>
+  </section>
+  <section
+    class="h-screen w-screen flex flex-col justify-evenly lg:grid lg:grid-cols-4 lg:grid-rows-1 lg:h-auto lg:items-center"
+  >
+    <img
+      class="object-contain object-center lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-2"
+      src="https://res.cloudinary.com/peggy-co/image/upload/f_auto,q_auto/v1596125701/Well%20Being/lady-field.min_jidrzu.jpg"
+      alt=""
+    />
+    <p
+      class="m-2 p-2 sm:m-4 sm:p-4 text-2xl leading-relaxed lg:col-start-3 lg:col-end-5 lg:row-start-1 lg:row-end-2"
+    >
+      I’m that guide. My desire today and every day hereon, is to make people
+      feel less broken, so they can experience more joy and happiness and less
+      stress in their lives. This in turn can only benefit those around them,
+      and there we have it, a beautiful ripple effect begins and reaches far and
+      wide, as far as the imagination can take you!
+    </p>
+  </section>
+  <section></section>
 </template>
