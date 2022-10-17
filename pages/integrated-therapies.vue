fad879ecd15b865e2d5d923556dc007d2e61e589
Peggy535 <peggy161152@gmail.com>
2022-10-17 17:17:17 +0100

    Changes to layour/pagaes and addition of jh shop

---
+++ pages/integrated-therapies.vue
@@ -0,0 +1,135 @@
+<script setup>
+	definePageMeta({
+		layout: "default",
+	});
+</script>
+<template>
+	<div>
+		<div class="overflow-hidden font-domainelight text-stone-600">
+			<section class="relative h-auto w-screen flex flex-col z-20 bg-stone-50">
+				<div class="w-full h-auto my-20 font-domainelight text-brand-rose-gold">
+					<h1 class="text-6xl sm:text-8xl md:text-11xl m-1 p-1">JH Devi</h1>
+					<h1 class="text-3xl sm:text-5xl m-1 p-1">Integrated Therapies</h1>
+				</div>
+
+				<div class="flex flex-col h-auto w-full sm:w-4/5 md:w-3/4 lg:w-2/3 mx-auto border-2 border-white bg-brand-rose-gold rounded-2xl">
+					<p class="text-3xl sm:text-4xl md:text-5xl m-2 p-2 text-white">
+						A platform created by JH to assist in opening up your very own unique gateway to understanding your true self. Your souls
+						fingerprint, it's very own navigation system buried deep within your energetic systems.
+					</p>
+				</div>
+
+				<div class="w-full sm:w-4/5 md:w-3/4 lg:w-2/3 mx-auto sm:mt-10">
+					<p class="m-2 p-2 text-xl md:text-2xl">
+						DEVI is an all embracing Mother Goddess. Heavenly Divine, anything of Excellence.
+					</p>
+					<p class="m-2 p-2 text-xl md:text-2xl">
+						A piece of DEVI is within us all, you just need to clear the ruble, the burdens you've collected and bring your magick closer
+						to the surface in order to shine brighter and lighter.
+					</p>
+				</div>
+
+				<div class="w-full sm:w-4/5 md:w-3/4 lg:w-2/3 mx-auto text-xl md:text-2xl">
+					<p class="m-2 p-2">
+						Welcome to your journey to finding YOU through me as your supportive guide and my integrated therapies. Each person is as
+						unique as their own fingerprint that's why each individuals session is designed specifically for them by integrating the best
+						fit therapies for each individual. This could be a combination of relaxation hypnosis and EFT or meditation and EFT, all three
+						or talk therapy and a little yoga
+					</p>
+				</div>
+			</section>
+			<section class="h-auto w-screen flex flex-col z-20 bg-stone-50">
+				<div class="flex flex-col w-full items-center">
+					<div class="w-full sm:w-4/5 mx-auto bg-brand-rose-gold rounded-2xl py-5">
+						<h1 class="m-1 p-1 text-5xl sm:text-6xl lg:text-8xl text-white">Hypnotherapy</h1>
+					</div>
+					<div class="flex flex-col m-1 p-1 w-full sm:w-4/5 sm:mx-auto lg:w-1/2">
+						<p class="m-2 p-4 bg-brand-rose-gold rounded-2xl text-white text-2xl lg:text-3xl">
+							Hypnotherapy is a natural therapy that can help you to cope with the problems and challenges of everyday life like
+							anxiety, phobias, fears, self esteem or even addiction to name but a few.
+						</p>
+						<p class="m-2 p-2">
+							Hypnotherapy is a natural therapy that can help you to cope with the problems and challenges of everyday life like
+							Anxiety, phobias or fears, self esteem, addiction ie smoking and Weightlose to name but a few. Curative is a more holistic
+							feel to the sessions and work we do together. The word 'hypnotherapy' is derived from ‘hypno’ - relating to ‘hypnosis’,
+							and ‘therapy’ because of the therapeutic suggestions that are given during treatment.
+						</p>
+						<p class="m-2 p-2">
+							Hypnosis is an enhanced state of relaxation. You are not asleep, but it is something like a state just before you drift
+							off to sleep - you can still hear everything, speak and even move around if you wished to. When you are in that state,
+							your subconscious mind is more accessible and open to suggestion.
+						</p>
+					</div>
+				</div>
+			</section>
+			<section class="h-auto w-screen flex flex-col z-20 bg-stone-50">
+				<div class="flex flex-col w-full items-center p-1">
+					<div class="w-full sm:w-4/5 mx-auto bg-brand-green rounded-2xl py-5">
+						<h1 class="text-5xl sm:text-6xl lg:text-8xl m-1 p-1 text-stone-500">EFT</h1>
+						<h2 class="text-lg sm:text-xl m-1 p-1 text-stone-500">Emotional Freedom Technique</h2>
+					</div>
+					<div class="flex flex-col w-full sm:w-4/5 mx-auto text-md m-1 p-1 lg:w-1/2">
+						<p class="m-2 p-4 bg-brand-green text-stone-500 text-2xl rounded-2xl">
+							At the cutting edge in the field of energy psychology is EFT (Emotional Freedom Techniques), which utilises the meridian
+							system an is therefore often known as meridian therapy or meridian psychotherapy.
+						</p>
+						<h3 class="text-2xl m-2 p-2">The Meridian System</h3>
+						<p class="m-2 p-2">
+							A basic theory of energy work is that the energy field, or aura body, contains energy structures and channels through
+							which the life force flows. The most well known of these are the chakras and the meridians. The chakras are often referred
+							to in metaphysical and spiritual work and are thought to act something like transformers, taking energy in one form and
+							converting it into another. The meridians are used in Traditional Chinese Medicine,(TCM) acupuncture, acupressure, chi
+							kung and reiki and are considered to be primary channels for the life force energy, much like an 'energy bloodstream'.
+						</p>
+						<h3 class="text-2xl m-2 p-2">How does it work?</h3>
+						<p class="m-2 p-2">
+							EFT tapping uses the same energy meridians in the body as acupuncture. When the flow of energy becomes blocked because of
+							pain, trauma or an experience, fears can start to grow. EFT helps to alter that flow of energy. We can work to process
+							those emotions through tapping on various parts of the face and body. matching that with positive statements, we can
+							transform our negative beliefs into positive ones. Using tapping in your daily routine can help with daily stresses and
+							anxieties. It can also ease chronic pain, treat addictions, eliminate phobias and help elevate allergies, the list is
+							endless.
+						</p>
+						<p class="m-2 p-2">
+							If you would like to see what the basic set up in EFT looks like just click the link and take a look at a short example
+							over on my youtube channel.
+						</p>
+					</div>
+				</div>
+			</section>
+			<section class="h-auto w-screen flex flex-col z-20 bg-stone-50">
+				<div class="flex flex-col w-full p-1 items-center">
+					<div class="w-full sm:w-4/5 mx-auto bg-brand-champagne-gold rounded-2xl py-5">
+						<h1 class="text-5xl sm:text-6xl lg:text-8xl text-stone-500 m-1 p-1">CBT</h1>
+						<h2 class="text-lg sm:text-xl m-1 p-1 text-stone-500">Cognitive Behavioral Therapy</h2>
+					</div>
+					<div class="flex flex-col w-full sm:w-4/5 mx-auto text-md m-1 p-1 lg:w-1/2">
+						<p class="m-2 p-4 bg-brand-champagne-gold text-stone-500 text-2xl rounded-2xl">
+							CBT is a therapy that focuses on the particular ways in which an individual thinks, behaves and acts, in the attempt to
+							deal successfully with their behavioural, emotional and social problems and issues.
+						</p>
+						<p class="m-2 p-2">
+							The difference between psychotherapy and behavioural therapy is that psychotherapy tends to emphasise the importance of
+							the personal meaning placed upon things and how the majority of thinking patterns begin in childhood. Behavioural therapy
+							relates to everything the person does. How they act with other people, when they are alone. How they avoid certain aspects
+							of life and how they engage and interact with the world around them. How active or inactive towards being involved in
+							participating in life on a healthy level.
+						</p>
+
+						<p class="m-2 p-2">
+							The great advantage that ‘CBT’ has over other therapies is that clients undergoing ‘CBT’ treatment are actually learning
+							the educational aspects of the therapy, which is then empowering them to become their own personal therapist and to be
+							more aware of the circumstances, situations, behaviours and people that could cause them to lapse and therefore to be able
+							to avoid these aspects in their lives. I bring together a combination of CBT and Curative Hypnosis to assist a number of
+							my clients and we have experienced amazing results. Especially in stress related disorders..
+						</p>
+						<p class="m-2 p-2">
+							I bring together a combination of CBT and Curative Hypnosis to assist a number of my clients and we have experienced
+							amazing results. Especially in stress related disorders.
+						</p>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+</template>
