8c6cf37dc38964a955589981b2ec2e2424457053
Peggy535 <peggy161152@gmail.com>
2022-11-03 10:08:43 +0000

    Changes to blog card styles

--- pages/jhshop.vue
+++ pages/jhshop.vue
@@ -2,7 +2,9 @@
 	<div>
 		<div class="flex flex-col h-screen w-screen text-brand-rose-gold font-domainemedium place-content-end bg-white">
 			<h1 class="text-8xl m-1 p-1">JH Shop</h1>
-			<h2 class="text-3xl m-1 p-1 mb-10">Discover the 12 Steps to you program, individual therapy audios and more.</h2>
+			<h2 class="text-xl m-1 p-1 mb-10">
+				Discover individual hypnotherapy Audios, life-changing programs and events to help you on your journey.
+			</h2>
 		</div>
 		<div class="flex flex-col md:flex-row md:flex-wrap md:flex-shrink w-screen h-auto font-domainemedium p-2 bg-white">
 			<div class="flex w-full md:w-1/2 md:p-2 lg:w-1/3">
@@ -24,10 +26,11 @@
 			<div class="flex w-full md:w-1/2 md:p-2 lg:w-1/3">
 				<article class="relative border border-brand-rose-gold rounded-2xl w-full sm:w-2/3 md:w-full m-2 mx-auto">
 					<div class="h-32 bg-brand-rose-gold rounded-xl text-white p-2">
-						<h1 class="text-4xl md:text-3xl lg:text-4xl xl:text-5xl">The 12-Step program</h1>
+						<h1 class="text-4xl md:text-3xl lg:text-4xl xl:text-5xl">JH Programs</h1>
 					</div>
 					<div class="relative m-1 p-1 text-brand-rose-gold h-48">
-						<p class="text-lg">Coming soon - Course starts Spring 2023.</p>
+						<p>Programs that are individually tailored, affordable & can tranform your life no matter what your goals are.</p>
+						<p class="text-lg">The 12 Steps to a new you - Spring 2023.</p>
 						<NuxtLink to="/jhshop">
 							<button class="absolute bottom-0 right-0 border border-brand-rose-gold rounded-3xl text-lg p-2">
 								<p class="text-xs md:text-sm lg:text-md xl:text-lg 2xl:text-xl">Discover more</p>
@@ -42,7 +45,10 @@
 						<h1 class="text-4xl md:text-3xl lg:text-4xl xl:text-5xl">JH Events</h1>
 					</div>
 					<div class="relative m-1 p-1 text-brand-sage h-48">
-						<p class="text-lg">A brief to the point description about events here</p>
+						<p class="text-lg">
+							Have you ever heard of a tapping circle? Why not get your friends together for a wholesome event or simply join a group.
+							It's fun, friendly and welcoming.
+						</p>
 						<NuxtLink to="/jhevents">
 							<button class="absolute bottom-0 right-0 border border-brand-sage rounded-3xl text-lg p-2">
 								<p class="text-xs md:text-sm lg:text-md xl:text-lg 2xl:text-xl">Discover more</p>
